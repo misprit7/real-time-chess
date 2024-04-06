1e3e455fa31837fb20591f6d9c7e9b2de3248218
misprit7 <xandernaumenko@gmail.com>
2024-04-05 18:18:47 -0700

    Added fft

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,125 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+// There are two speedup options for some of the FFT code:
+
+// Define this to use reciprocal multiplication for division and some more speedups that might decrease precision
+//#define FFT_SPEED_OVER_PRECISION
+
+// Define this to use a low-precision square root approximation instead of the regular sqrt() call
+// This might only work for specific use cases, but is significantly faster. Only works for ArduinoFFT<float>.
+//#define FFT_SQRT_APPROXIMATION
+
+#include "arduinoFFT.h"
+#include "core_pins.h"
+
+/*
+These values can be changed in order to evaluate the functions
+*/
+#define CHANNEL A9
+const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
+const float samplingFrequency = 5000; //Hz, must be less than 10000 due to ADC
+unsigned int sampling_period_us;
+unsigned long microseconds;
+
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+float vReal[samples];
+float vImag[samples];
+
+float freq[samples/2];
+
+/* Create FFT object with weighing factor storage */
+ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency, true);
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
+
+void PrintVector(float *vData, uint16_t bufferSize, uint8_t scaleType)
+{
+    for (uint16_t i = 0; i < bufferSize; i++)
+    {
+        float abscissa;
+        /* Print abscissa value */
+        switch (scaleType)
+        {
+            case SCL_INDEX:
+                abscissa = (i * 1.0);
+        break;
+            case SCL_TIME:
+                abscissa = ((i * 1.0) / samplingFrequency);
+        break;
+            case SCL_FREQUENCY:
+                abscissa = ((i * 1.0 * samplingFrequency) / samples);
+        break;
+        }
+        Serial.print(abscissa, 6);
+        if(scaleType==SCL_FREQUENCY)
+            Serial.print("Hz");
+        Serial.print(" ");
+        Serial.println(vData[i], 4);
+    }
+    Serial.println();
+}
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(22, OUTPUT);
+    sampling_period_us = round(1000000*(1.0/samplingFrequency));
+    Serial.begin(115200);
+    while (!Serial); 
+    Serial.println("Ready");
+
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+    /* Serial.println(analogRead(CHANNEL)); */
+    /* delay(10); */
+    /* return; */
+    /*SAMPLING*/
+    microseconds = micros();
+    for(int i=0; i<samples; i++)
+    {
+        vReal[i] = analogRead(CHANNEL);
+        vImag[i] = 0;
+        while(micros() - microseconds < sampling_period_us){
+            //empty loop
+        }
+        microseconds += sampling_period_us;
+    }
+    /* Print the results of the sampling according to time */
+    /* Serial.println("Data:"); */
+    /* PrintVector(vReal, samples, SCL_TIME); */
+    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
+    FFT.dcRemoval();
+    /* Serial.println("Weighed data:"); */
+    /* PrintVector(vReal, samples, SCL_TIME); */
+    FFT.compute(FFTDirection::Forward); /* Compute FFT */
+    /* Serial.println("Computed Real values:"); */
+    /* PrintVector(vReal, samples, SCL_INDEX); */
+    /* Serial.println("Computed Imaginary values:"); */
+    /* PrintVector(vImag, samples, SCL_INDEX); */
+    FFT.complexToMagnitude(); /* Compute magnitudes */
+    /* Serial.println("Computed magnitudes:"); */
+    /* PrintVector(vReal, (samples >> 1), SCL_FREQUENCY); */
+    /* float x = FFT.majorPeak(); */
+    /* Serial.println(x, 6); //Print out what frequency is the most dominant. */
 
-  // wait for a second
-  delay(1000);
+    float max = 0;
+    int maxi = 0;
+    for(int i = 5; i < samples/2; ++i){
+        if(vReal[i] > max){
+            maxi = i;
+            max = vReal[i];
+        }
+    }
+    float df = samplingFrequency / samples;
+    Serial.println(maxi * df, 6);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
 
-   // wait for a second
-  delay(1000);
+    delay(100);
 }
